7bd4339a0bd77eef0168a124bf9082fa60b4b648
accountonline <jermain2johnson@gmail.com>
2025-02-10 18:59:04 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Handle form submission
-document.getElementById('contactForm')?.addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for contacting us! We will get back to you soon.');
-  e.target.reset();
-});
